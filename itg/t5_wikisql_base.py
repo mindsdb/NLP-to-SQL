d3f56aa9e30aef55212913e7c45db8b7a799e469
george <george@cerebralab.com>
2021-12-14 14:26:54 -0400

    almost done /w trainable hugging face model

--- itg/t5_wikisql_base.py
+++ itg/t5_wikisql_base.py
@@ -1,21 +1,25 @@
+from typing import Tuple, List
 from transformers import AutoModelWithLMHead, AutoTokenizer
+from controllers import Prompt
 
 
 class T5WS()
     def __init__(self):
-        pass
-tokenizer = AutoTokenizer.from_pretrained("mrm8488/t5-base-finetuned-wikiSQL")
-model = AutoModelWithLMHead.from_pretrained("mrm8488/t5-base-finetuned-wikiSQL")
+        self.tokenizer = AutoTokenizer.from_pretrained("mrm8488/t5-base-finetuned-wikiSQL")
+        self.model = AutoModelWithLMHead.from_pretrained("mrm8488/t5-base-finetuned-wikiSQL")
 
-def get_sql(query):
-  input_text = "translate English to SQL: %s </s>" % query
-  features = tokenizer([input_text], return_tensors='pt')
+    def _prepare(self, prompt: Prompt, query: str = None):
+        features = self.tokenizer([prompt.to_text()], return_tensors='pt')
+        output = None
+        if query is not None:
+            output = self.tokenizer([output], return_tensors='pt')
+        return features, output
 
-  output = model.generate(input_ids=features['input_ids'], 
-               attention_mask=features['attention_mask'])
-  
-  return tokenizer.decode(output[0])
+    def __call__(self, prompt: Prompt) -> str:
+        features, _ = self._prepare(prompt)
+        output = self.model.generate(input_ids=features['input_ids'], attention_mask=features['attention_mask'])
+        return self.tokenizer.decode(output[0])
 
-query = "How many models were finetuned using BERT as base model?"
-
-get_sql(query)
+    def train(self, training_data: List[Tuple[Prompt, str]]):
+        for prompt, query in training_data:
+            features, output = self._prepare(prompt, query)
